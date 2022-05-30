e0539283ce8c58b9bc2184356b8c92681b672454
Emma <eqbrown@isf.edu.hk>
2022-05-30 08:59:59 +0800

    update repo for 21/22

--- game.py
+++ game.py
@@ -3,5 +3,26 @@
 #
 # This file should run your game.
 #
-# You can (and should) create other files and modules to import them
-# here as needed.
+######################################################################
+
+from quest.game import QuestGame
+from quest.map import TiledMap
+from quest.dialogue import Dialogue
+from quest.modal import Modal, DialogueModal
+from quest.sprite import QuestSprite, Player, Wall, NPC
+from quest.helpers import scale, resolve_resource_path
+from quest.strategy import RandomWalk
+import arcade
+import os
+from pathlib import Path
+
+class Game(QuestGame):
+    # 💻 YOUR CODE GOES HERE 💻 #
+    # Hint: You may want to look at how the example games are set up #
+    
+    
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
